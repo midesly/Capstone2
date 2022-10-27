49c534a2c62ca2672946df1c406a4316aec80b52
midesly <ayofrancesakin@gmail.com>
2022-10-27 19:53:00 -0400

    mountain and national js in progress

---
+++ scripts/mountain.js
@@ -0,0 +1,22 @@
+function mountainImage(item){
+    const img = document.createElement("img");
+    img.src = "images/" + item.img;
+    return img;
+}
+
+function mountainTitle(item){
+    const text = document.createElement("div");
+    text.classList.add("card-title");
+    text.innerHTML = item.name;
+    return text;
+}
+
+function mountainCard(item) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = item.name;
+    card.innerHTML += `<img src="images/${item.img}">`;    
+    card.appendChild(mountainImage(item));    
+    card.appendChild(mountainTitle(item));
+    return card;
+}
