2e4b4f4eae0fa982cc3c2fb651b511ae28ef0e98
midesly <ayofrancesakin@gmail.com>
2022-10-28 09:54:27 -0400

    ommit

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
-const navURLs= [
-    {label: "Home", url: "index.html"},
-    {label: "National Parks", url: "parks.html"},
-    {label: "Mountains", url: "mountains.html"},
+const navURLs = [
+    {name: "Home", url: "index.html"},
+    {name: "National Parks", url: "parks.html"},
+    {name: "Mountains", url: "mountains.html"},
 ];
 
-
+//helper/factory function makes a new DOM Element
+function navLink(item){
+    const a = document.createElement("a");
+    a.href = item.url;
+    a.innerHTML = item.name;
+    return a;
+}
 document.addEventListener("DOMContentLoaded", () => {
-    const nav= document.getElementById("nav");
-    navURLs.forEach(item => nav.innerHTML += `<a href="${item.url}">${item.label}</a>`);
-    
+    const nav = document.getElementById("nav");
+    //innerHTML += appending strings from javascript template ``
+    // navURLs.forEach(item => nav.innerHTML += `<a href="${item.url}">${item.name}</a>`);
+    navURLs.forEach(item => nav.appendChild(navLink(item)));
 });
